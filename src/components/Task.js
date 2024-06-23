5d2262ee59ae11db3d7968019bac5641d390888e
Hannah Sanati <hannahsanatiii@gmail.com>
2024-06-23 14:26:00 +0330

    fixing the bell icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,48 +4,32 @@ import { AiFillDelete } from "react-icons/ai";
 import { FaBell } from "react-icons/fa";
 
 const Task = ({ task, onDelete, onToggle, onEdit, Reminder }) => {
-  // const isDone = `task ${task.done ? "done" : ""}`;
-
   return (
-    // <div
-    //   className={`task ${task.reminder ? "reminder" : ""}`}
-    //   ${} a condition
-    //   onDoubleClick={() => onToggle(task.id)}
-    // >
-    
-    <div 
-    className={`task ${task.reminder ? "reminder" : ""}`}
-    >
-
-    <div
-      onDoubleClick={() => onToggle(task.id)}
-      className={`task ${task.isDone ? "done" : ""}`}
-      // style={{ borderLeft: task.done ? "4px solid green" : "none" }}
-    >
-      <h5 >
-        {task.text}{" "}
-        <FaBell
-          onClick={() => Reminder(task.id)}
-        />
-        <FaEdit
-          onClick={() => onEdit(task.id)}
-          style={{
-            color: "mediumseagreen",
-            cursor: "pointer",
-          }}
-        />
-        <AiFillDelete
-          onClick={() => onDelete(task.id)}
-          style={{
-            color: "indianred",
-            cursor: "pointer",
-          }}
-        />{" "}
-      </h5>
-      <p>{task.day}</p>
+    <div className={`task ${task.reminder ? "reminder" : ""}`}>
+      <div
+        onDoubleClick={() => onToggle(task.id)}
+        className={`task ${task.isDone ? "done" : ""}`}
+      >
+        <h5>
+          {task.text} <FaBell onClick={() => Reminder(task.id)} />
+          <FaEdit
+            onClick={() => onEdit(task.id)}
+            style={{
+              color: "mediumseagreen",
+              cursor: "pointer",
+            }}
+          />
+          <AiFillDelete
+            onClick={() => onDelete(task.id)}
+            style={{
+              color: "indianred",
+              cursor: "pointer",
+            }}
+          />{" "}
+        </h5>
+        <p>{task.day}</p>
+      </div>
     </div>
-    </div>
-
   );
 };
 
